84ad453edb28d2a3b9e73e4337b105dfd636d872
caychan <cxl_cxlcxl@sina.com>
2022-03-23 22:07:05 +0800

    test controller

---
+++ src/main/java/com/cay/rockstock/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.cay.rockstock.controller;
+
+
+import com.cay.rockstock.beans.CommonResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @GetMapping(value = "/test")
+    public CommonResponse startSpider() {
+        log.info("receive test request");
+        return CommonResponse.success();
+    }
+}
